276172f8312f4a6f8ca0d820b49a7384dd0dd670
Michael Schroeder <mls@suse.de>
2014-11-13 15:17:42 +0100

    fix misuse of memset (thanks dcb314)

--- sign.c
+++ sign.c
@@ -738,7 +738,7 @@ static void md5_final(byte *digest, struct MD5Context *ctx)
     byteReverse((unsigned char *) ctx->buf, 4);
 #endif
     memcpy(digest, ctx->buf, 16);
-    memset(ctx, 0, sizeof(ctx));        /* In case it's sensitive */
+    memset(ctx, 0, sizeof(*ctx));        /* In case it's sensitive */
 }
 
 /* The four core functions - F1 is optimized somewhat */
