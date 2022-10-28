f3de702bf607be337977213a5a289f2f1177a453
Michael Schroeder <mls@suse.de>
2022-10-28 12:15:16 +0200

    Update copyright

--- sign.c
+++ sign.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2013 Michael Schroeder, Novell Inc.
+ * Copyright (c) 2006-2022 SUSE LLC
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
